452a710eeb652e7fd0dffbbb325222729c7b17f1
umut.ulupinar <umutulupinarrr@gmail.com>
2024-10-11 01:03:05 +0300

    initial wrapping - scrapper

---
+++ Wrapping/Mackolik/ScrapperService.cs
@@ -0,0 +1,17 @@
+﻿namespace FantasyFootballSquadBot.Wrapping.Mackolik;
+
+public class ScrapperService
+{
+    private readonly WebScrapper _webScraper;
+
+    public ScrapperService()
+    {
+        _webScraper = new WebScrapper();
+    }
+    
+    public string GetTitle(string url) =>
+        _webScraper.ScrapeWebsite(url).Result;
+        
+    
+    
+}
---
+++ Wrapping/Mackolik/WebScrapper.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+
+namespace FantasyFootballSquadBot.Wrapping.Mackolik;
+
+public class WebScrapper
+{
+    private readonly HttpClient _httpClient;
+
+    public WebScrapper()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string> ScrapeWebsite(string url)
+    {
+        var html = await _httpClient.GetStringAsync(url);
+
+        HtmlDocument document = new HtmlDocument();
+        document.LoadHtml(html);
+        
+        var titleNode = document.DocumentNode.SelectSingleNode("//title");
+        return titleNode?.InnerText ?? "Başlık bulunamadı";
+        
+    }
+}
